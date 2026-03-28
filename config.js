e72ddff86fbe1f61b0ab682a5957795294833cc1
Earladuma <earltrevor@gmail.com>
2026-03-28 17:09:30 +0300

    clean production version

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+ apiKey: "AIzaSyD5Su_t5eRiVc3jV_1v417ZjpAuGja-prA",
+  authDomain: "kerichpharma.firebaseapp.com",
+  projectId: "kerichpharma",
+  storageBucket: "kerichpharma.firebasestorage.app",
+  messagingSenderId: "668206751685",
+  appId: "1:668206751685:web:cc097cff689269378375ac",
+  measurementId: "G-QDRBJ6S9PZ"
+};
